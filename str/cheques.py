c7cd984a791beed868c4d597f84356b21288d701
Adm_Fideisa <cartera@fideisainversion.com.ar>
2025-10-17 12:04:32 -0300

    Calculadoras de los costos de tomar deuda con los inversores de la BCBB o la venta de pagares.

---
+++ str/cheques.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from pyxirr import xirr
+
+from str.constantes import gast_dep_chq, com_gst_bco
+
+def deuda_bcbb(tna: float, comision: float, plazo: int, capital: float = 10**6, fecha: pd.Period = pd.Period.now('D'), periodos: int = 12) -> pd.DataFrame:
+     
+    cap = capital
+    
+    df = []
+    
+    for p in range(periodos):
+        vto = fecha + plazo
+        f_com = pd.Period(fecha, freq='M') + 1
+        f_com = pd.Period(year=f_com.year, month=f_com.month, day=10, freq='D')
+        com = capital * comision/365 * plazo
+        interes = capital * tna/365 * plazo
+        total = capital + interes
+        df.append({
+            'Emisión': fecha,
+            'Vto.' : vto,
+            'Capital': capital,
+            'Interés': interes,
+            'Total'  : capital + interes,
+            'Pago Comisión': f_com,
+            'Comisión': com
+        })
+        fecha = vto
+        capital = total
+    
+    df = pd.DataFrame(df)
+    df.index.name = 'Periodo'
+    
+    return df
+
+def flujo(tna: float, comision: float, plazo: int, capital: float = 10**6, fecha: pd.Period = pd.Period.now('D'), periodo: int =12) -> tuple[pd.DataFrame, float, float]:
+    
+    periodos = min(periodo, int(365 / plazo), 12)
+    df_bcbb = deuda_bcbb(tna, comision, plazo, capital, fecha, periodos)
+    print(f'Comisiones y Gts. Dep. Chq.: {gast_dep_chq:.2%}')
+    print(f'Comisiones y Gts. Bco.: {com_gst_bco:.2%}\n')
+    
+    print(f'Capital: $ {capital:,.0f}')
+    print(f'TNA: {tna:.2%}')
+    print(f'Comisiones BCBB: {comision:.2%}')
+    print(f'Plazo: {plazo}\n')
+    
+    fechas = list(df_bcbb['Emisión']) + list(df_bcbb['Vto.']) + list(df_bcbb['Pago Comisión'])
+    fechas = {'Fecha': fechas}
+    df = pd.DataFrame(fechas)
+    df = df.drop_duplicates().sort_values(by='Fecha').reset_index(drop=True)
+    df.set_index('Fecha', inplace=True)
+    df['Flujo'] = 0.0
+    
+    df.loc[fecha, 'Flujo'] -= capital * (1 - gast_dep_chq) * (1 - com_gst_bco)**2 # type: ignore
+    for i in df_bcbb.index:
+        f_pago = df_bcbb.at[i, 'Pago Comisión']
+        comision = df_bcbb.at[i, 'Comisión'] # type: ignore
+        df.loc[f_pago, 'Flujo'] += comision * (1 + com_gst_bco) # type: ignore
+        
+    ult_vto = max(df_bcbb['Vto.'])
+    df.loc[ult_vto, 'Flujo'] += df_bcbb.loc[periodos-1, 'Total'] * (1 + com_gst_bco)**2 # type: ignore
+    df.index = df.index.to_timestamp() # type: ignore
+    
+    tea = float(xirr(dates=df.index, amounts=df['Flujo'])) # type: ignore
+    tem = (1+tea)**(30/365) - 1 # type: ignore
+    
+    print(f'TEM: {tem:.2%}')
+    print(f'TEA: {tea:.2%}')
+    
+    return df, tea, tem
---
+++ str/pagares.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+
+from pyxirr import xirr
+
+from str.constantes import com_gst_bco, d_mercado, arancel
+
+
+def pagares(tamar: float, tna: float, plazo:int, capital: float = 10**6, fecha: pd.Period = pd.Period.now('D'), periodos: int = 12) -> pd.DataFrame:
+    
+    df = []
+    precio = 1 / (1 + (tna/365 * plazo))
+    der_mer = d_mercado * min(plazo,90) / 90 * 1.21
+    gst_arancel = arancel * min(plazo,365) / 365 * 1.21
+    print(f'Precio: {precio:.6f}, Der. Mercado: {der_mer}, Gst. Arancel: {gst_arancel}')
+    
+    for i in range(periodos):
+        vto = fecha + plazo
+        descontado = capital * precio
+        gastos = descontado * (der_mer + gst_arancel)
+        neto = descontado - gastos
+        interes = neto * tamar * plazo / 365
+        total = capital + interes
+        df.append({
+            'Emisión'   : fecha,
+            'Vto.'      : vto,
+            'Capital'   : capital,
+            'Descontado': descontado,
+            'Gastos'    : gastos,
+            'Neto'      : neto,
+            'Interés'   : interes,
+            'Total'     : total})
+        
+        capital = np.ceil(total * (1 + com_gst_bco) / 10**5) * 10**5
+        fecha = vto
+    
+    df = pd.DataFrame(df)
+    
+    return df
+
+
+def flujo(tamar: float, tna: float, plazo: int, capital: float = 10**6, fecha: pd.Period = pd.Period.now('D'), periodo: int =12) -> tuple[pd.DataFrame, float, float]:
+   
+    periodos = min(periodo, int(365 / plazo), 12)
+    df_pagares = pagares(tamar, tna, plazo, capital, fecha, periodos)
+    
+    print(f'Capital: $ {capital:,.0f}')
+    print(f'TAMAR: {tamar:.2%}')
+    print(f'TNA: {tna:.2%}')
+    print(f'Plazo: {plazo}\n')
+    
+    fechas = list(df_pagares['Emisión']) + list(df_pagares['Vto.'])
+    fechas = {'Fecha': fechas}
+    df = pd.DataFrame(fechas)
+    df = df.drop_duplicates().sort_values(by='Fecha').reset_index(drop=True)
+    df.set_index('Fecha', inplace=True)
+    df['Flujo'] = 0.0
+    
+    df.loc[fecha, 'Flujo'] -= df_pagares.at[0, 'Neto'] * (1 - com_gst_bco) # type: ignore
+    for i in range(periodos-1):
+        vto = df_pagares.at[i, 'Vto.']
+        neto = df_pagares.at[i + 1, 'Neto']
+        total = df_pagares.at[i, 'Total']
+        df.at[vto, 'Flujo'] += total * (1+com_gst_bco) - neto * (1 - com_gst_bco) # type: ignore
+    
+    ult_vto = max(df_pagares['Vto.'])
+    df.loc[ult_vto, 'Flujo'] += df_pagares.at[periodos-1, 'Total'] * (1 + com_gst_bco) # type: ignore
+    
+    df.index = df.index.to_timestamp() # type: ignore
+    
+    tea = float(xirr(dates=df.index, amounts=df['Flujo'])) # type: ignore
+    tem = (1+tea)**(30/365) - 1
+    
+    print(f'TEM: {tem:.2%}')
+    print(f'TEA: {tea:.2%}')
+    
+    return df, tea, tem
